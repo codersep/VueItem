61905fab298d5bde6f07a8729ec35c81734da385
misted <2285088054@qq.com>
2020-11-11 15:58:30 +0800

    axios

--- vue-axios/src/main.js
+++ vue-axios/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
@@ -7,4 +8,15 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+
+axios({
+  url: 'http://47.98.96.69:8080',
+  method: "get",
+  params: {
+    word:'a'
+  }
+}).then(function(res){
+  console.log(res);
 })
